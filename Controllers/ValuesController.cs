fa4b4f9115ad6cfc8d02e0896e7cf52adbafd522
CreatorsSquad <90002175+CreatorsSquad@users.noreply.github.com>
2021-10-17 14:17:08 +0530

    valuescontroller

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreatorsSquad.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hi", "MICROSOFT DOTNET CORE 3.1 WEBAPI" };
+        }
+    }
+}
